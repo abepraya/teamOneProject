29b9afcd47ccd6cf5b6056eb51bfe4836060eaab
steady00 <steady.tjiakra@gmail.com>
2022-02-16 16:08:48 +0700

    tambah Queue Ticket Activity

---
+++ app/src/main/java/id/bagusip/projectkel1/Konfigurasi.java
@@ -0,0 +1,17 @@
+package id.bagusip.projectkel1;
+
+public class Konfigurasi {
+
+    public static final String URL_ALL_QUEUE_TICKET_DEVELOPER = "http://192.168.0.6/teamOneProject/dashboard_dev/datas_queue_ticket.php";
+
+
+    public static final String TAG_JSON_ARRAY = "result";
+
+    public static final String TAG_JSON_ID_TICKET = "id_ticket";
+    public static final String TAG_JSON_CREATE_DATE = "create_date";
+    public static final String TAG_JSON_NAME_EMP = "name_emp";
+    public static final String TAG_JSON_PROBLEM_NAME = "problem_name";
+
+    public static final String TICKET_ID = "ticket_id";
+
+}
