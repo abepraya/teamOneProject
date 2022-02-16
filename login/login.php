a7ec346287e83cbfbb47cd70f9106008da1939c5
abepraya <muhammadjadidabipraya@gmail.com>
2022-02-16 13:52:26 +0700

    "added login validation & access screen"

---
+++ login/login.php
@@ -0,0 +1,115 @@
+<?php
+if($_SERVER['REQUEST_METHOD']=='POST'){
+    require_once('koneksi.php');
+
+    $email = $_POST['email'];  
+    $password = $_POST['password'];  
+      
+    $sql = "SELECT e.id_emp, e.email, e.name_emp, a.password, d.role, e.job_title
+    FROM account a JOIN employee e ON a.id_emp = e.id_emp
+    JOIN division d ON e.id_division = d.id_division
+    WHERE e.email = '$email' and a.password = '$password';";  
+
+//Mendapatkan Hasil
+$r = mysqli_query($con,$sql);
+	
+//Membuat Array Kosong 
+$result = array();
+
+$row = mysqli_fetch_array($r);
+
+function generate_jwt($headers, $payload, $secret = 'secret') {
+	$headers_encoded = base64url_encode(json_encode($headers));
+	
+	$payload_encoded = base64url_encode(json_encode($payload));
+	
+	$signature = hash_hmac('SHA256', "$headers_encoded.$payload_encoded", $secret, true);
+	$signature_encoded = base64url_encode($signature);
+	
+	$jwt = "$headers_encoded.$payload_encoded.$signature_encoded";
+	
+	return $jwt;
+}
+
+function base64url_encode($str) {
+    return rtrim(strtr(base64_encode($str), '+/', '-_'), '=');
+}
+
+function is_jwt_valid($jwt, $secret = 'secret') {
+	// split the jwt
+	$tokenParts = explode('.', $jwt);
+	$header = base64_decode($tokenParts[0]);
+	$payload = base64_decode($tokenParts[1]);
+	$signature_provided = $tokenParts[2];
+
+	// check the expiration time - note this will cause an error if there is no 'exp' claim in the jwt
+	$expiration = json_decode($payload)->exp;
+	$is_token_expired = ($expiration - time()) < 0;
+
+	// build a signature based on the header and payload using the secret
+	$base64_url_header = base64url_encode($header);
+	$base64_url_payload = base64url_encode($payload);
+	$signature = hash_hmac('SHA256', $base64_url_header . "." . $base64_url_payload, $secret, true);
+	$base64_url_signature = base64url_encode($signature);
+
+	// verify it matches the signature provided in the jwt
+	$is_signature_valid = ($base64_url_signature === $signature_provided);
+	
+	if ($is_token_expired || !$is_signature_valid) {
+		return FALSE;
+	} else {
+		return TRUE;
+	}
+}
+
+
+if(mysqli_num_rows($r) == 1){
+    $temp_email = $row['email'];
+    $temp_password = $row['password'];
+    $temp_role = $row['role'];
+
+    if($email == $temp_email && $password == $temp_password){
+        $headers = array('alg'=>'HS256','typ'=>'JWT');
+        $payload = array('sub'=>$row['id_emp'],'name'=>$row['name_emp'], 'admin'=>true, 'exp'=>(time() + 60));
+        
+        $jwt = generate_jwt($headers, $payload);
+
+        array_push($result, array(
+            "status" => "success",
+            "message" => "Login Success",
+            "name_emp"=> $row['name_emp'],
+            "email"=> $row['email'],
+            "role"=> $row['role'],
+            "job_title" => $row['job_title'],
+            "access_token" => $jwt
+        ));
+    }
+    else if($email != $temp_email || $password != $temp_password){
+        array_push($result,array(
+            "status" => "failed",
+            "message"=>"invalid email & password"
+        ));
+    }
+}
+else if(empty($email) || empty($password)){
+    array_push($result,array(
+        "status" => "failed",
+        "message"=>"Please input email and password"
+    ));
+}
+
+else{
+    array_push($result,array(
+        "status" => "failed",
+        "message"=>"invalid email & password"
+    ));
+}
+
+
+//Menampilkan Array dalam Format JSON
+echo json_encode(array('result'=>$result));
+
+mysqli_close($con);
+}  
+
+?>
