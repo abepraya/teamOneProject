074e8a365ae4cc464151de364840f9178c29a985
Oktasanti Puteri <98955318+oktasantiputri@users.noreply.github.com>
2022-02-16 14:13:27 +0700

    login and register form

--- app/src/main/java/id/bagusip/projectkel1/MainActivity.java
+++ app/src/main/java/id/bagusip/projectkel1/MainActivity.java
@@ -1,21 +1,59 @@
 package id.bagusip.projectkel1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.textfield.TextInputLayout;
+
+import id.bagusip.projectkel1.Dashboard.DashboardEmployeeActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    TextInputLayout textInputLayoutEmail, textInputLayoutPass;
+    CardView cardViewLogin;
+    TextView txtSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textInputLayoutEmail = findViewById(R.id.txtEmailLogin);
+        textInputLayoutPass = findViewById(R.id.txtPassLogin);
+        cardViewLogin = findViewById(R.id.btnLogin);
+        txtSignUp = findViewById(R.id.txtSignUp);
+
+        txtSignUp.setOnClickListener(this);
+        cardViewLogin.setOnClickListener(this);
+
+
+
     }
 
     public void onClick (View v){
-        Intent i = new Intent(this, RegisterActivity.class);
-        startActivity(i);
+        String email = textInputLayoutEmail.getEditText().getText().toString();
+        String password = textInputLayoutPass.getEditText().getText().toString();
+
+        if(v == txtSignUp)
+        {
+            Intent i = new Intent(this, RegisterActivity.class);
+            startActivity(i);
+        }
+        else if (v == cardViewLogin)
+        {
+            Log.d("test",email);
+            Toast.makeText(this, email, Toast.LENGTH_SHORT).show();
+           Intent i = new Intent(this, DashboardEmployeeActivity.class);
+           startActivity(i);
+        }
+
     }
 }
--- app/src/main/java/id/bagusip/projectkel1/RegisterActivity.java
+++ app/src/main/java/id/bagusip/projectkel1/RegisterActivity.java
@@ -1,14 +1,94 @@
 package id.bagusip.projectkel1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class RegisterActivity extends AppCompatActivity {
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.Calendar;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+    TextInputLayout textEmployeeName, textEmployeeEmail, textEmployeeAddress,
+            textEmployeePassword, textEmployeePhone, textDatePickerOfBirth,
+            textEmployeeJobTitle;
+
+    EditText dateOfBirth;
+    Spinner spinnerIDDivision, spinnerIDBranch;
+    DatePickerDialog.OnDateSetListener dateOfBirthPicker;
+    CardView cardViewRegister;
+    TextView txtLogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        textEmployeeName = findViewById(R.id.employeeName);
+        textEmployeeEmail = findViewById(R.id.employeeEmail);
+        textEmployeeAddress = findViewById(R.id.employeeAddress);
+        textEmployeePassword = findViewById(R.id.employeePassword);
+        textDatePickerOfBirth = findViewById(R.id.datePickerOfBirth);
+        textEmployeeJobTitle = findViewById(R.id.employeeJobTitle);
+        textEmployeePhone = findViewById(R.id.employeePhone);
+        dateOfBirth = findViewById(R.id.dateOfBirth);
+        cardViewRegister = findViewById(R.id.btnRegister);
+
+        Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        dateOfBirth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        RegisterActivity.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth,dateOfBirthPicker,year,month,day
+                );
+                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                datePickerDialog.show();
+            }
+
+        });
+
+        cardViewRegister.setOnClickListener(this);
+
+        dateOfBirthPicker = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month = month+1;
+                String date = year+"-"+month+"-"+dayOfMonth;
+                dateOfBirth.setText(date);
+            }
+        };
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        String nama = textEmployeeName.getEditText().getText().toString();
+        String email = textEmployeeEmail.getEditText().getText().toString();
+        String address = textEmployeeAddress.getEditText().getText().toString();
+        String password = textEmployeePassword.getEditText().getText().toString();
+        String phone = textEmployeePhone.getEditText().getText().toString();
+        String date_of_birth = textDatePickerOfBirth.getEditText().getText().toString();
+        String job_title = textEmployeeJobTitle.getEditText().getText().toString();
+        if(view == cardViewRegister){
+            Toast.makeText(getApplicationContext(), address, Toast.LENGTH_SHORT).show();
+            Log.d("res",nama+"\n"+email+"\n"+address+"\n"+password+"\n"+phone+"\n"+date_of_birth+"\n"+job_title);
+        }
     }
 }
