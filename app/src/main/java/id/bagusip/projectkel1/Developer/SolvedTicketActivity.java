ad5084dcca6a4bbb2fc0d56fdc11de6ee214d9d3
Rusheila <rusheila@gmail.com>
2022-02-16 20:11:57 +0700

    solved ticket

---
+++ app/src/main/java/id/bagusip/projectkel1/Developer/SolvedTicketActivity.java
@@ -0,0 +1,16 @@
+package id.bagusip.projectkel1.Developer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import id.bagusip.projectkel1.R;
+
+public class SolvedTicketActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_solved_ticket);
+    }
+}
