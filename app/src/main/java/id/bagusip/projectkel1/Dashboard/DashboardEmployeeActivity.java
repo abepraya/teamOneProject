b6fecf2b0a26d86bdb327cf4c80a99e6a4909eb1
Rusheila <rusheila@gmail.com>
2022-02-16 11:12:43 +0700

    update Dashboard by donna

---
+++ app/src/main/java/id/bagusip/projectkel1/Dashboard/DashboardEmployeeActivity.java
@@ -0,0 +1,17 @@
+package id.bagusip.projectkel1.Dashboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import id.bagusip.projectkel1.R;
+
+public class DashboardEmployeeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard_employee);
+
+    }
+}
