7c28956a1f515e7fb93dbb3ba23fe858af63b86a
bagusip <bagusindrt@gmail.com>
2022-02-17 14:11:59 +0700

    view ongoing ticket

--- app/src/main/java/id/bagusip/projectkel1/Employee/MyTicketActivity.java
+++ app/src/main/java/id/bagusip/projectkel1/Employee/MyTicketActivity.java
@@ -1,16 +1,67 @@
 package id.bagusip.projectkel1.Employee;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import id.bagusip.projectkel1.Dashboard.DashboardEmployeeActivity;
 import id.bagusip.projectkel1.R;
 
-public class MyTicketActivity extends AppCompatActivity {
+public class MyTicketActivity extends AppCompatActivity implements View.OnClickListener{
+    CardView cardQueueTicket, cardOngoingTicket, cardSolvedTicket;
+    String role, access_token, email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_ticket);
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+
+        role = extras.getString("role");
+        access_token = extras.getString("access_token");
+        email = extras.getString("email");
+
+        cardQueueTicket = findViewById(R.id.cardQueueTicket);
+        cardOngoingTicket = findViewById(R.id.cardOngoingTicket);
+        cardSolvedTicket = findViewById(R.id.cardSolvedTicket);
+
+        cardQueueTicket.setOnClickListener(this);
+        cardOngoingTicket.setOnClickListener(this);
+        cardOngoingTicket.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == cardQueueTicket){
+            Intent intent = new Intent(MyTicketActivity.this, CreateTicketActivity.class);
+            Bundle extras = new Bundle();
+            extras.putString("role",role);
+            extras.putString("access_token",access_token);
+            extras.putString("email",email);
+            intent.putExtras(extras);
+            startActivity(intent);
+        }
+        else if(view == cardOngoingTicket){
+            Intent intent = new Intent(MyTicketActivity.this, OnGoingTicketEmployeeActivity.class);
+            Bundle extras = new Bundle();
+            extras.putString("role",role);
+            extras.putString("access_token",access_token);
+            extras.putString("email",email);
+            intent.putExtras(extras);
+            startActivity(intent);
+        }
+        else if(view == cardSolvedTicket){
+            Intent intent = new Intent(MyTicketActivity.this, CreateTicketActivity.class);
+            Bundle extras = new Bundle();
+            extras.putString("role",role);
+            extras.putString("access_token",access_token);
+            extras.putString("email",email);
+            intent.putExtras(extras);
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/id/bagusip/projectkel1/Employee/OnGoingTicketEmployeeActivity.java
+++ app/src/main/java/id/bagusip/projectkel1/Employee/OnGoingTicketEmployeeActivity.java
@@ -2,16 +2,107 @@ package id.bagusip.projectkel1.Employee;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import id.bagusip.projectkel1.R;
+import id.bagusip.projectkel1.config.HttpHandler;
+import id.bagusip.projectkel1.config.Konfigurasi;
 
 public class OnGoingTicketEmployeeActivity extends AppCompatActivity {
+    String role, access_token, email, temp_json, JSON_STRING;
+    ListView list_on_going_ticket;
+    private ProgressDialog loading;
+    private View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_on_going_ticket_employee);
+        getJsonData();
+    }
+
+    private void getJsonData() {
+        class GetJsonData extends AsyncTask<Void, Void, String> {
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(view.getContext(), "Ambil Data Instruktur", "Harap menunggu...", false, false);
+            }
+
+            @Override
+            protected String doInBackground(Void... voids) {
+                HashMap<String, String> ongoing = new HashMap<>();
+                ongoing.put(Konfigurasi.KEY_NAMA_EMAIL, email);
+                ongoing.put(Konfigurasi.KEY_TICKET_STATUS, "Assigned");
+                HttpHandler handler = new HttpHandler();
+                String result = handler.sendPostRequest(Konfigurasi.URL_GET_TICKET,ongoing);
+                return result;
+            }
+
+            @Override
+            protected void onPostExecute(String message) {
+                super.onPostExecute(message);
+                loading.dismiss();
+                JSON_STRING = message;
+                Log.d("DATA_JSON: ", JSON_STRING);
+                // Toast.makeText(view.getContext(), JSON_STRING, Toast.LENGTH_LONG).show();
+
+                // menampilkan data json kedalam list view
+                displayAllDataOnGoingTicket();
+            }
+        }
+        GetJsonData getJsonData = new GetJsonData();
+        getJsonData.execute();
+    }
+
+    private void displayAllDataOnGoingTicket() {
+        JSONObject jsonObject = null;
+        ArrayList<HashMap<String, String>> list = new ArrayList<HashMap<String, String>>();
+
+        try {
+            jsonObject = new JSONObject(JSON_STRING);
+            JSONArray jsonArray = jsonObject.getJSONArray(Konfigurasi.TAG_JSON_ARRAY);
+
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject object = jsonArray.getJSONObject(i);
+                String id_ticket = object.getString("t.id_ticket");
+                String nama_emp = object.getString("e.name_emp");
+                String create_date = object.getString("t.create_date");
+                String assign_date = object.getString("t.assign_date");
+
+
+                HashMap<String, String> ongoing_ticket = new HashMap<>();
+                ongoing_ticket.put(Konfigurasi.KEY_TICKET_ID, id_ticket);
+                ongoing_ticket.put(Konfigurasi.KEY_NAMA_EMP, nama_emp);
+                ongoing_ticket.put(Konfigurasi.KEY_TICKET_CREATED_DATE, create_date);
+                ongoing_ticket.put(Konfigurasi.KEY_TICKET_ASSIGNED_DATE, assign_date);
+
+                list.add(ongoing_ticket);
+
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+//        ListAdapter adapter = new SimpleAdapter(
+//                view.getContext(), list, R.layout.activity_on_going_ticket_employee,
+//                new String[]{"id_pst", "nama_pst"},
+//                new int[]{R.id.txt_id, R.id.txt_name}
+//        );
+
 
     }
 }
