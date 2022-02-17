13283117a02ca3f38bbca2121c389ade8573fb21
Oktasanti Puteri <98955318+oktasantiputri@users.noreply.github.com>
2022-02-17 14:27:06 +0700

    Add solved and queue ticket list and details layout

---
+++ app/src/main/java/id/bagusip/projectkel1/Employee/SolvedTicketEmployeeActivity.java
@@ -0,0 +1,16 @@
+package id.bagusip.projectkel1.Employee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import id.bagusip.projectkel1.R;
+
+public class SolvedTicketEmployeeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_solved_ticket_employee);
+    }
+}
